0466a391ce78f481386a4d13728e590098b3ff3c
Thomas Rose <thomas.rose@daad-alumni.de>
2021-10-03 19:37:19 +0200

    updated to new U238/U235

--- calculate_model_ages.R
+++ calculate_model_ages.R
@@ -1,18 +1,21 @@
 #' Calculate lead isotope age models
-#' 
+#'
 #' @author Thomas Rose, \email{thomas.rose@daad-alumni.de}
 #'
-#' This function provides a convenient way to calculate the different age
-#' models implemented with \code{\link{Stacey_Kramers_1975}},
-#' \code{\link{Cumming_Richards_1975}}, and \code{\link{Albarede_et_al_2012}}
-#' (see \emph{References} for the respective publications).
+#'   This function provides a convenient way to calculate the different age
+#'   models implemented with \code{\link{Stacey_Kramers_1975}},
+#'   \code{\link{Cumming_Richards_1975}}, and \code{\link{Albarede_et_al_2012}}
+#'   (see \emph{References} for the respective publications). The updated
+#'   238U/235U ratio of 137.79 (Goldmann et al. 2015) is used in all functions
+#'   instead of the original value 138.88 because this is the now commonly
+#'   accepted value.
 #'
-#' The used model is indicated in the column names of the output by the
-#' abbreviations given above.
+#'   The used model is indicated in the column names of the output by the
+#'   abbreviations given above.
 #'
-#' Although not necessary for the model of Cumming and Richards 1975, the
-#' 208Pb/204Pb ratio must also be provided for consistency of the input with the
-#' other age model functions.
+#'   Although not necessary for the model of Cumming and Richards 1975, the
+#'   208Pb/204Pb ratio must also be provided for consistency of the input with
+#'   the other age model functions.
 #'
 #' @param ratio_206_204 A character vector or data frame column with the
 #'   206Pb/204Pb ratio.
@@ -23,31 +26,36 @@
 #' @param model Character string with the abbreviation of the model to
 #'   calculate: \itemize{ \item \code{"SK75"} for Stacey and Kramers 1975 \item
 #'   \code{"CR75"} for Cumming and Richards 1975 \item \code{"AJ84"} for
-#'   AlbarÃ¨de and Juteau 1984 \item \code{"ADB12"} for
-#'   AlbarÃ¨de et al. 2012 \item \code{"all"} for all models at once.}
+#'   AlbarÃ¨de and Juteau 1984 \item \code{"ADB12"} for AlbarÃ¨de et al. 2012
+#'   \item \code{"all"} for all models at once.}
 #'
 #' @return A data frame.
 #' @export
-#' 
 #'
-#' @references
-#' Albarède, F., Desaulty, A.-M. and Blichert-Toft, J. (2012) A
-#' geological perspective on the use of Pb isotopes in Archaeometry,
-#' Archaeometry, 54(5), pp. 853‒867.
-#' \url{https://dx.doi.org/10.1111/j.1475-4754.2011.00653.x}.
 #'
-#' Albarède, F. and Juteau, M. (1984) Unscrambling the lead model ages,
-#' Geochimica et Cosmochimica Acta, 48(1), pp. 207‒212.
-#' \url{https://dx.doi.org/10.1016/0016-7037(84)90364-8}.
+#' @references Albarède, F., Desaulty, A.-M. and Blichert-Toft, J. (2012) A
+#'   geological perspective on the use of Pb isotopes in Archaeometry,
+#'   Archaeometry, 54(5), pp. 853‒867.
+#'   \url{https://dx.doi.org/10.1111/j.1475-4754.2011.00653.x}.
 #'
-#' Cumming, G.L. and Richards, J.R. (1975) Ore lead isotope ratios in a
-#' continuously changing earth, Earth and Planetary Science Letters, 28(2), pp.
-#' 155‒171. \url{https://dx.doi.org/10.1016/0012-821X(75)90223-X}.
+#'   Albarède, F. and Juteau, M. (1984) Unscrambling the lead model ages,
+#'   Geochimica et Cosmochimica Acta, 48(1), pp. 207‒212.
+#'   \url{https://dx.doi.org/10.1016/0016-7037(84)90364-8}.
 #'
-#' Stacey, J.S. and Kramers, J.D. (1975) Approximation of terrestrial lead
-#' isotope evolution by a two-stage model, Earth and Planetary Science Letters,
-#' 26(2), pp. 207‒221. \url{https://dx.doi.org/10.1016/0012-821X(75)90088-6}.
+#'   Cumming, G.L. and Richards, J.R. (1975) Ore lead isotope ratios in a
+#'   continuously changing earth, Earth and Planetary Science Letters, 28(2),
+#'   pp. 155‒171. \url{https://dx.doi.org/10.1016/0012-821X(75)90223-X}.
 #'
+#'   Goldmann, A., Brennecka, G., Noordmann, J., Weyer, S. and Wadhwa, M. (2015)
+#'   The uranium isotopic composition of the Earth and the Solar System,
+#'   Geochimica et Cosmochimica Acta, 148,
+#'   pp. 145–158.\url{https://dx.doi.org/10.1016/j.gca.2014.09.008}
+#'
+#'   Stacey, J.S. and Kramers, J.D. (1975) Approximation of terrestrial lead
+#'   isotope evolution by a two-stage model, Earth and Planetary Science
+#'   Letters, 26(2), pp. 207‒221.
+#'   \url{https://dx.doi.org/10.1016/0012-821X(75)90088-6}.
+#'   
 
 LI_model_age <- function(ratio_206_204, ratio_207_204, ratio_208_204, model = c("SK75", "CR75", "AJ84", "ADB12", "all"))
 {
@@ -75,20 +83,22 @@ LI_model_age <- function(ratio_206_204, ratio_207_204, ratio_208_204, model = c(
 #'
 #' @author Thomas Rose, \email{thomas.rose@daad-alumni.de}
 #'
-#' These functions calculate the age model and their respective mu and kappa
-#' values according to the publications they are named after (see
-#' \emph{References}.
+#'   These functions calculate the age model and their respective mu and kappa
+#'   values according to the publications they are named after (see
+#'   \emph{References}. The updated 238U/235U ratio of 137.79 (Goldmann et al.
+#'   2015) is used in all functions instead of the original value 138.88 because
+#'   this is the now commonly accepted value.
 #'
-#' The used model is indicated in the column names of the output by the
-#' initials of the author's last names and the publication year (e. g. SK75
-#' for Stacey & Kramers, 1975).
+#'   The used model is indicated in the column names of the output by the
+#'   initials of the author's last names and the publication year (e. g. SK75
+#'   for Stacey & Kramers, 1975).
 #'
-#' The ratio of 208Pb/204Pb is not necessary for \link{Cumming_Richards_1975}.
-#' The function takes it as argument only to be consistent with the input of
-#' the other age model functions. If provided, it will be ignored.
+#'   The ratio of 208Pb/204Pb is not necessary for \link{Cumming_Richards_1975}.
+#'   The function takes it as argument only to be consistent with the input of
+#'   the other age model functions. If provided, it will be ignored.
 #'
-#' The function for the age model of Albarède & Juteau 1984 is based on the
-#' MATLAB-script of F. Albarède (version 2020-11-06).
+#'   The function for the age model of Albarède & Juteau 1984 is based on the
+#'   MATLAB-script of F. Albarède (version 2020-11-06).
 #'
 #' @param ratio_206_204 A character vector or data frame column with the
 #'   206Pb/204Pb ratio.
@@ -101,23 +111,28 @@ LI_model_age <- function(ratio_206_204, ratio_207_204, ratio_208_204, model = c(
 #'
 #' @export
 #'
-#' @references
-#' Albarède, F., Desaulty, A.-M. and Blichert-Toft, J. (2012) A
-#' geological perspective on the use of Pb isotopes in Archaeometry,
-#' Archaeometry, 54(5), pp. 853‒867.
-#' \url{https://dx.doi.org/10.1111/j.1475-4754.2011.00653.x}.
+#' @references Albarède, F., Desaulty, A.-M. and Blichert-Toft, J. (2012) A
+#'   geological perspective on the use of Pb isotopes in Archaeometry,
+#'   Archaeometry, 54(5), pp. 853‒867.
+#'   \url{https://dx.doi.org/10.1111/j.1475-4754.2011.00653.x}.
 #'
-#' Albarède, F. and Juteau, M. (1984) Unscrambling the lead model ages,
-#' Geochimica et Cosmochimica Acta, 48(1), pp. 207‒212.
-#' \url{https://dx.doi.org/10.1016/0016-7037(84)90364-8}.
+#'   Albarède, F. and Juteau, M. (1984) Unscrambling the lead model ages,
+#'   Geochimica et Cosmochimica Acta, 48(1), pp. 207‒212.
+#'   \url{https://dx.doi.org/10.1016/0016-7037(84)90364-8}.
 #'
-#' Cumming, G.L. and Richards, J.R. (1975) Ore lead isotope ratios in a
-#' continuously changing earth, Earth and Planetary Science Letters, 28(2), pp.
-#' 155‒171. \url{https://dx.doi.org/10.1016/0012-821X(75)90223-X}.
+#'   Cumming, G.L. and Richards, J.R. (1975) Ore lead isotope ratios in a
+#'   continuously changing earth, Earth and Planetary Science Letters, 28(2),
+#'   pp. 155‒171. \url{https://dx.doi.org/10.1016/0012-821X(75)90223-X}.
 #'
-#' Stacey, J.S. and Kramers, J.D. (1975) Approximation of terrestrial lead
-#' isotope evolution by a two-stage model, Earth and Planetary Science Letters,
-#' 26(2), pp. 207‒221. \url{https://dx.doi.org/10.1016/0012-821X(75)90088-6}.
+#'   Goldmann, A., Brennecka, G., Noordmann, J., Weyer, S. and Wadhwa, M. (2015)
+#'   The uranium isotopic composition of the Earth and the Solar System,
+#'   Geochimica et Cosmochimica Acta, 148,
+#'   pp. 145–158.\url{https://dx.doi.org/10.1016/j.gca.2014.09.008}
+#'   
+#'   Stacey, J.S. and Kramers, J.D. (1975) Approximation of terrestrial lead
+#'   isotope evolution by a two-stage model, Earth and Planetary Science
+#'   Letters, 26(2), pp. 207‒221.
+#'   \url{https://dx.doi.org/10.1016/0012-821X(75)90088-6}.
 #'
 #' @name age_models
 #' @aliases Stacey_Kramers_1975
@@ -137,12 +152,13 @@ Stacey_Kramers_1975 <- function(ratio_206_204, ratio_207_204, ratio_208_204)
   l238 <- 0.155125 * 10^-9 # decay constant 238U
   l235 <- 0.98485 * 10^-9  # decay constant 235U
   l232 <- 0.049475 * 10^-9 # decay constant 232Th
+  U238235 <- 137.79
   
   # Defining model age function
   
   Model_age_func <- function(x,y) {
     stats::uniroot(function(X, a, b) {
-      ((exp(l235*t0)-exp(l235*X))/(137.88*(exp(l238*t0)-exp(l238*X))))-((b0-b)/(a0-a))
+      ((exp(l235*t0)-exp(l235*X))/(U238235*(exp(l238*t0)-exp(l238*X))))-((b0-b)/(a0-a))
     },
     a = x, b = y,
     interval = c(-10000*10^6, t0),
@@ -184,12 +200,13 @@ Cumming_Richards_1975 <- function(ratio_206_204, ratio_207_204, ratio_208_204) #
   Wp <- 41.25 # present day 232Th/204Pb
   e1 <- 0.05 * 10^-9 # epsilon parameter (growth rate mu)
   e2 <- 0.037 * 10^-9 # epsilon apostrophe parameter (growth rate kappa)
+  U238235 <- 137.79
   
   # Defining model age function
   
   Model_age_func <- function(x,y) {
     stats::optimize(function(X, a, b) {
-      (a0 - a + 137.88*Vp*((exp(l238*t0)*(1 - e1*(t0 - 1/l238)))-(exp(l238*X)*(1 - e1*(X - 1/l238)))))^2 + (b0 - b + Vp*((exp(l235*t0)*(1 - e1*(t0 - 1/l235)))-(exp(l235*X)*(1 - e1*(X - 1/l235)))))^2
+      (a0 - a + U238235*Vp*((exp(l238*t0)*(1 - e1*(t0 - 1/l238)))-(exp(l238*X)*(1 - e1*(X - 1/l238)))))^2 + (b0 - b + Vp*((exp(l235*t0)*(1 - e1*(t0 - 1/l235)))-(exp(l235*X)*(1 - e1*(X - 1/l235)))))^2
     },
     a = x, b = y,
     interval = c(-10000*10^6,t0),
@@ -202,7 +219,7 @@ Cumming_Richards_1975 <- function(ratio_206_204, ratio_207_204, ratio_208_204) #
   
   Model_Age <- replace(Model_Age, Model_Age <= -10000*10^6 + 1*10^6 | Model_Age >= t0- 1*10^6, NA)
   
-  mu <- 137.88*Vp*(1-e1*Model_Age)
+  mu <- U238235*Vp*(1-e1*Model_Age)
   kappa <- Wp*(1-e2*Model_Age)/mu
   
   result <- data.frame("Model_Age_CR75" = Model_Age * 10^-6, "mu_CR75" = mu, "kappa_CR75" = kappa)
@@ -283,13 +300,14 @@ Albarede_et_al_2012 <- function(ratio_206_204, ratio_207_204, ratio_208_204)
   l238 <- 0.155125 * 10^-9 # decay constant 238U
   l235 <- 0.98485 * 10^-9  # decay constant 235U
   l232 <- 0.049475 * 10^-9 # decay constant 232Th
+  U238235 <- 137.79
   
   # Defining model age function
   
   Model_age_func <- function(x,y) {
     stats::uniroot(function(X, a, b) {
-      ((b-bp)/(a-ap)) - ((1/137.88)*((exp(l235*t0)-exp(l235*X))/((exp(l238*t0)-exp(l238*X))))) -
-        (((mup*(exp(l238*X)-1))/(137.88*(a-ap)))*(((exp(l235*t0)-exp(l235*X))/(exp(l238*t0)-exp(l238*X))) - 
+      ((b-bp)/(a-ap)) - ((1/U238235)*((exp(l235*t0)-exp(l235*X))/((exp(l238*t0)-exp(l238*X))))) -
+        (((mup*(exp(l238*X)-1))/(U238235*(a-ap)))*(((exp(l235*t0)-exp(l235*X))/(exp(l238*t0)-exp(l238*X))) - 
                                                     ((exp(l235*X)- 1)/(exp(l238*X)- 1))))
     },
     a = x, b = y,
